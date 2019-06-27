49a14fe2159ebc17b6237cbdd8d3b0aac4251e1c
jpullinaquaq <joel.pullin@aquaq.co.uk>
2019-06-27 10:53:41 +0100

    Sorting out query and adding buttons

--- webdev/src/components/HDBCounts.js
+++ webdev/src/components/HDBCounts.js
@@ -3,6 +3,7 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import axios from 'axios';
+import * as d3 from "d3";
 
 
 class HDBCounts extends Component {
@@ -12,10 +13,14 @@ class HDBCounts extends Component {
         super(props);
         this.dataStore=[];
         this.state = {
+            dataStore:[],
+            symbol: 'AAPL'
+        };
+        this.state = {
             columnDefs: [{
-                headerName: "colCount", field: "colCount", sortable: true, filter: true, resizable: true
-            }, {
                 headerName: "Date", field: "date", sortable: true, filter: true, resizable: true
+            }, {
+                headerName: "Number of trades", field: "colCount", sortable: true, filter: true, resizable: true
             }],
 
             dataStore:[]
@@ -23,6 +28,22 @@ class HDBCounts extends Component {
         }
         this.updateData();
     };
+
+    changeSym = (sym) => {
+        console.log(sym);
+        this.setState({symbol: sym});
+        this.updateData();
+    };
+
+    componentDidMount() {
+        this.interval= setInterval(() =>  this.updateData(), 1000);
+    }
+
+    componentWillUnmount() {
+        d3.selectAll("svg > *").remove();
+        clearInterval(this.interval);
+    }
+
 
     options = {
         url: 'https://192.168.1.57:8140/executeQuery',
@@ -48,7 +69,7 @@ class HDBCounts extends Component {
     }
 
     updateData() {
-        this.getData("select colCount:count i by date from trade where date >.z.d-7")
+        this.getData(`select colCount:count i by date from trade where ((date >.z.d-8)&(1<date mod 7)),sym=\`${this.state.symbol}`)
             .then(data => {
                 if (data.success) {
                     console.log("data success=true");
@@ -62,8 +83,42 @@ class HDBCounts extends Component {
 
         return (
             <React.Fragment>
+
                 <div>
-                    SymVol
+                    <button className='nav-buttons' onClick={() =>this.changeSym("AAPL")}>
+                        AAPL
+                    </button>
+                    <button className='nav-buttons' onClick={() =>this.changeSym('AIG')}>
+                        AIG
+                    </button>
+                    <button className='nav-buttons' onClick={() =>this.changeSym('AMD')}>
+                        AMD
+                    </button>
+                    <button className='nav-buttons' onClick={() =>this.changeSym('DELL')}>
+                        DELL
+                    </button>
+                    <button className='nav-buttons' onClick={() =>this.changeSym('DOW')}>
+                        DOW
+                    </button>
+                    <button className='nav-buttons' onClick={() =>this.changeSym('GOOG')}>
+                        GOOG
+                    </button>
+                    <button className='nav-buttons' onClick={() =>this.changeSym('HPQ')}>
+                        HPQ
+                    </button>
+                    <button className='nav-buttons' onClick={() =>this.changeSym('IBM')}>
+                        IBM
+                    </button>
+                    <button className='nav-buttons' onClick={() =>this.changeSym('INTC')}>
+                        INTC
+                    </button>
+                    <button className='nav-buttons' onClick={() =>this.changeSym('MSFT')}>
+                        MSFT
+                    </button>
+
+                </div>
+                <div>
+                   {this.state.symbol}
                 </div>
                 <div
                     className="ag-theme-balham"
